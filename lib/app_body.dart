cd02a0c5c28f915ea1eb9b20fa9fd10b94bbce2c
Takiul ISlam Sumon <takiulislam20@gmail.com>
2024-09-01 01:18:51 +0600

    Filterchip

--- lib/app_body.dart
+++ lib/app_body.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 class homepage extends StatefulWidget {
@@ -10,13 +12,26 @@ class homepage extends StatefulWidget {
 class _homepageState extends State<homepage> {
   TextEditingController search_product = TextEditingController();
   final List<String> filters = <String>['All', 'Adidas', 'Nike', 'Bata', 'Bay'];
+  bool isSelected = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-      child: Container(),
+        child: Container(
+            child: FilterChip(
+          label: const Center(
+            child: Text('Chip Widget'),
+          ),
+          selected: isSelected,
+          onSelected: (value) {
+            setState(() {
+              isSelected = !isSelected;
+            });
+          },
+          // avatar: Text('A'),
+        )),
       ),
     );
   }
